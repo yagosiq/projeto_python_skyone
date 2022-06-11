cf9165775005eb8853fe9e3998ca3ca55fbd9b5c
YagoSiq <yagosiqti@gmail.com>
2022-06-11 19:03:13 -0300

    Criação da classe Pessoa e Squad

---
+++ .vscode/cadastro-de-squads.py
@@ -0,0 +1,10 @@
+class Pessoa: 
+    def __init__(self, nome, fone):
+        self.nome = nome
+        self.fone = fone
+
+class Squad: 
+    def __init__(self, nome, techlead=None, devs=None):
+        self.nome = nome
+        self.devs = []
+        self.techlead = techlead
