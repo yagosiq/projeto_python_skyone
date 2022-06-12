ca38498abd31a9504e9d822869e8fa9bdea14853
Yago Siqueira <yagosiqti@gmail.com>
2022-06-12 07:40:42 -0300

    Classe Colaborador e Dev utilizando herança criados

--- sly.solutions/cadastro-de-squad.py
+++ sly.solutions/cadastro-de-squad.py
@@ -13,3 +13,13 @@ class Squad:
         
     def incluir_techlead(self, techlead):
     self.techlead = techlead
+
+class Colaborador(Pessoa):
+    def __init__(self, nome, fone, squad=None):
+        super().__init__(nome, fone)
+        self.squad = squad
+
+class Dev(Colaborador):
+    def __init__(self, nome, fone, cargo, squad=None):
+        super().__init__(nome, fone, squad)
+        self.cargo = cargo
